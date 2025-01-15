8f2254657eb013b01ca119fa48105d4470d43eb1
SecretIGO <110442934+SecretIGO@users.noreply.github.com>
2025-01-15 16:05:53 +0800

    [Create_Base_UI]
    Refactor: Removed unnecessary/unused imports
    Refactor: Used new directory of ProfileScreen

--- portfoliowebsite/src/index.js
+++ portfoliowebsite/src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ProfileScreen from './dev/screens/profile/ProfileScreen';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ProfileScreen from './dev/screens/ProfileScreen';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
